4317addb9439c033e2840ddb41cfd80c2fb92561
miker169 <miker169@hotmail.co.uk>
2013-04-19 15:37:40 +0100

    Added Knockout view and view model and added a stylesheet for the main view

--- SimpleContactManager/App_Start/BundleConfig.cs
+++ SimpleContactManager/App_Start/BundleConfig.cs
@@ -17,13 +17,18 @@ namespace SimpleContactManager
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                "~/Scripts/knockout-2.2.0"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
+  bundles.Add(new StyleBundle("~/Content/css").Include("~/Content"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/site.css",
+                "~/Content/contact.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
